891726387438105896adf71766c1f10316f6e65a
cowkami <lee.unseo17@gmail.com>
2024-09-14 12:58:16 +0900

    test failed

--- black-body/tests/lib.rs
+++ black-body/tests/lib.rs
@@ -5,6 +5,13 @@ use rstest::rstest;
 #[case(1.0)]
 #[should_panic]
 #[case(-1.0)]
-fn calc_wave_length(#[case] temperature: f64) {
+fn new(#[case] temperature: f64) {
     let _ = BlackBody::new(temperature);
+}
+
+#[rstest]
+#[case(5800.0, 5.0)]
+fn calc_wave_length(#[case] temperature: f64, #[case] wave_length: f64) {
+    let body = BlackBody::new(temperature);
+    assert_eq!(body.calc_wave_length(), wave_length);
 }
